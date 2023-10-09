97d180a4b5e7257ffddc133f568aa999b21ed85f
Ghosal <ananya.ghosal@capgemini.com>
2023-10-10 02:18:18 +0530

    RA-9 updated

--- DBExcelproject/src/main/java/com/excel/demo/project/entity/SourceData.java
+++ DBExcelproject/src/main/java/com/excel/demo/project/entity/SourceData.java
@@ -3,11 +3,11 @@ package com.excel.demo.project.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @Entity
@@ -16,18 +16,16 @@ public class SourceData {
 	
 	
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "GGID")
-	private double gGId;
+	private int gGId;
 	
 	@Column(name = "VGID")
 	private double vGCrewId;
 	
-	@Column(name = "SOWID")
-	private String sowId;
-	
 	@Column(name = "RESOURCE_NAME")
 	private String resourceName;
-	
+
 	@Column(name = "LEVEL")
 	private String level;
 	
@@ -39,9 +37,9 @@ public class SourceData {
 	
 	@Column(name = "PO")
 	private String po;
-
-	@Column(name = "COMMENT")
-	private String comment;
+	
+	@Column(name = "SOWID")
+	private String sowId;
 	
 	@Column(name = "HOURS")
 	private String hours;
@@ -52,17 +50,20 @@ public class SourceData {
 	@Column(name = "AMOUNT")
 	private String amount;
 	
-	@Column(name = "CONTRACT_AMOUNT")
-	private String totalContractAmount;
-	
 	@Column(name = "ROLE_STRTDT")
 	private String roleStartDate;
 	
 	@Column(name = "ROLE_ENDDT")
 	private String roleEndDate;
 	
-	@Column(name = "LOCATION")
-	private String location;
+	@Column(name = "CONTRACT_AMOUNT")
+	private String totalContractAmount;
+
+	@Column(name = "COMMENT")
+	private String comment;
+	
+	@Column(name = "STATUS")
+	private String status;
 
 	  
 }
--- DBExcelproject/src/main/java/com/excel/demo/project/helper/SourceHelper.java
+++ DBExcelproject/src/main/java/com/excel/demo/project/helper/SourceHelper.java
@@ -63,7 +63,7 @@ public class SourceHelper {
 					{
 					case 0:
 						cell.setCellType(CellType.NUMERIC);
-						sourcedata.setGGId(cell.getNumericCellValue());
+						sourcedata.setGGId((int)cell.getNumericCellValue());
 						break;
 					case 1:
 						cell.setCellType(CellType.NUMERIC);
@@ -71,61 +71,60 @@ public class SourceHelper {
 						break;
 					case 2:
 						cell.setCellType(CellType.STRING);
-						sourcedata.setSowId(cell.getStringCellValue());
+						sourcedata.setResourceName(cell.getStringCellValue());
 						break;
 					case 3:
 						cell.setCellType(CellType.STRING);
-						sourcedata.setResourceName(cell.getStringCellValue());
+						sourcedata.setLevel(cell.getStringCellValue());
 						break;
 					case 4:
 						cell.setCellType(CellType.STRING);
-						sourcedata.setLevel(cell.getStringCellValue());
-						break;
+						sourcedata.setJobRole(cell.getStringCellValue());
+						break;	
 					case 5:
 						cell.setCellType(CellType.STRING);
-						sourcedata.setJobRole(cell.getStringCellValue());
-						break;
-					case 6:
-						cell.setCellType(CellType.STRING);
 						sourcedata.setSkill(cell.getStringCellValue());
-						break;
-					case 7:
+						break;		
+					case 6:
 						cell.setCellType(CellType.STRING);
 						sourcedata.setPo(cell.getStringCellValue());
 						break;
-					case 8:
+					case 7:
 						cell.setCellType(CellType.STRING);
-						sourcedata.setComment(cell.getStringCellValue());
-						break;
-					case 9:
+						sourcedata.setSowId(cell.getStringCellValue());
+						break;	
+					case 8:
 						cell.setCellType(CellType.STRING);
 						sourcedata.setHours(cell.getStringCellValue());
 						break;
-					case 10:
+					case 9:
 						cell.setCellType(CellType.STRING);
 						sourcedata.setHourlyRate(cell.getStringCellValue());
 						break;
-					case 11:
+					case 10:
 						cell.setCellType(CellType.STRING);
 						sourcedata.setAmount(cell.getStringCellValue());
 						break;
-					case 12:
-						cell.setCellType(CellType.STRING);
-						sourcedata.setTotalContractAmount(cell.getStringCellValue());
-						break;
-					case 13:
+					case 11:
 						cell.setCellType(CellType.STRING);
 						sourcedata.setRoleStartDate(cell.getStringCellValue());
 						break;
+					case 12:
+						cell.setCellType(CellType.STRING);
+						sourcedata.setRoleEndDate(cell.getStringCellValue());
+						break;
+					case 13:
+						cell.setCellType(CellType.STRING);
+						sourcedata.setTotalContractAmount(cell.getStringCellValue());
+						break;
 					case 14:
 						cell.setCellType(CellType.STRING);
-						sourcedata.setRoleEndDate(null);
+						sourcedata.setComment(cell.getStringCellValue());
 						break;
 					case 15:
 						cell.setCellType(CellType.STRING);
-						sourcedata.setLocation(cell.getStringCellValue());
+						sourcedata.setStatus(cell.getStringCellValue());
 						break;
-					
 					default:
 						break;
 					
