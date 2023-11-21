389a5f14ac2bcd1179b140a69f3f2f5db53dd02c
subhog23 <125849547+subhog23@users.noreply.github.com>
2023-11-21 11:16:37 +0530

    Update VGVdiDetailHelper.java

--- reportautomation/src/main/java/com/vg/resource/reportautomation/helper/VGVdiDetailHelper.java
+++ reportautomation/src/main/java/com/vg/resource/reportautomation/helper/VGVdiDetailHelper.java
@@ -1,12 +1,12 @@
 package com.vg.resource.reportautomation.helper;
 
 import java.io.InputStream;
-import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
- 
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
@@ -14,10 +14,11 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.multipart.MultipartFile;
- 
+
 import com.vg.resource.reportautomation.entity.VGVdiDetailEntity;
- 
+
 public class VGVdiDetailHelper {
+	
 	public static boolean checkExcelFormat(MultipartFile file)
 	{
 		String contentType = file.getContentType();
@@ -27,6 +28,7 @@ public class VGVdiDetailHelper {
 		}
 		return false;
 	}
+	
 	public static List<VGVdiDetailEntity> convertExceltoList(InputStream is)
 	{
 		List<VGVdiDetailEntity> list = new ArrayList<>();
@@ -35,24 +37,22 @@ public class VGVdiDetailHelper {
 			DataFormatter formatter = new DataFormatter();
 			Workbook workbook = new XSSFWorkbook(is);
 			Sheet sheet = workbook.getSheetAt(0);
-			for (Cell cell : sheet.getRow(0)) {
-				requiredHeaders.put(cell.getStringCellValue(), cell.getColumnIndex());
+			for (Cell cell : sheet.getRow(0)) {				
+				requiredHeaders.put(cell.getStringCellValue().toUpperCase().trim(), cell.getColumnIndex());
 			}
 			for (int i = 1; i <= sheet.getLastRowNum(); i++) {
 				Row row = sheet.getRow(i);
-				VGVdiDetailEntity vdidata = new VGVdiDetailEntity();
-				//vdidata.setVdi_ggid(formatter.formatCellValue(row.getCell(requiredHeaders.get("GGID"))));
-				//vdidata.setId(formatter.formatCellValue(row.getCell(requiredHeaders.get("ID"))));
-				vdidata.setResource_name(formatter.formatCellValue(row.getCell(requiredHeaders.get("Resource Name"))));
-				//vdidata.setVg_email_id(formatter.formatCellValue(row.getCell(requiredHeaders.get("VG Email ID"))));
-				vdidata.setVdi_name(formatter.formatCellValue(row.getCell(requiredHeaders.get("VDI name"))));
-				vdidata.setOdc_location(formatter.formatCellValue(row.getCell(requiredHeaders.get("ODC Location"))));
-				vdidata.setStatus(formatter.formatCellValue(row.getCell(requiredHeaders.get("Status"))));
-				//vdidata.setLwd((Date) row.getCell(requiredHeaders.get("LWD")));	
+				VGVdiDetailEntity vdidata = new VGVdiDetailEntity();							
+				vdidata.setVdi_ggid(formatter.formatCellValue(row.getCell(requiredHeaders.get("ID"))).toString());
+				vdidata.setResource_name(formatter.formatCellValue(row.getCell(requiredHeaders.get("RESOURCE NAME"))));
+				vdidata.setVg_email_id(formatter.formatCellValue(row.getCell(requiredHeaders.get("VG EMAIL ID"))));
+				vdidata.setVdi_name(formatter.formatCellValue(row.getCell(requiredHeaders.get("VDI NAME"))));
+				vdidata.setStatus(formatter.formatCellValue(row.getCell(requiredHeaders.get("STATUS"))));
+				vdidata.setOdc_location(formatter.formatCellValue(row.getCell(requiredHeaders.get("ODC LOCATION"))));				
+				vdidata.setLwd(formatter.formatCellValue(row.getCell(requiredHeaders.get("LWD"))));				
 				list.add(vdidata);
-
- 
 			}
+			
 			workbook.close();
 		}
 		catch(Exception e)
